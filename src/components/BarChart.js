840e00638ef781902fef871f040e4846d9ac3130
Devin Davis <DLC@devin-daviss-macbook-pro-729.local>
2019-06-02 15:45:19 -0400

    console logging data. attempting to render chart data

--- src/components/BarChart.js
+++ src/components/BarChart.js
@@ -1,15 +1,76 @@
 import React, { Component } from 'react';
 import Chart from 'chart.js'
 
+// fetch('/tonus_eqs')
+// .then(response => response.json())
+// .then(json => console.log(json))
+// .catch(err => console.log(err))
+
 class BarChart extends Component {
+    componentDidMount () {
+        this.getData()
+    }
+
+    getData() {
+        fetch('/tonus_eqs')
+        .then(response => response.json())
+        // .then(json => console.log(json))
+        // .catch(err => console.log(err))
+        .then(jData => this.preparedData(jData))  
+        .then(data => this.createChart(data))
+    }
+
+    createChart(data) {
+        const ctx = document.querySelector('#moods')
+        console.log(ctx);
+        const moodsChart = new Chart(ctx, {
+            type: 'bar',
+            data: data
+        })
+    }
+
+    preparedData (data) {
+        const chartData = {
+            labels: [],
+            datasets: [
+                {
+                    label: '',
+                    data: [],
+                    fill: false,
+                    lineTension: 0,
+                },
+
+            ]
+        }
+
+    // START OF FOR LOOP
+    data.forEach( (genre, i) => {
+        console.log(data);
+    // MUSIC CHART LABEL
+    chartData.datasets[i].label = genre
+
+
+    // START OF NESTED LOOP (MOOD DATA)
+    genre.moods.forEach((moods, j) => {
+    chartData.labels.push(moods.mood_state)
+    
+    chartData.datasets[i].data.push(moods.mood_state)
+
+    })
+    })
+
+    return chartData;
+
+}
+
   render () {
     return (
       <>
         <h1>Tonus Mood EQ</h1>
-        <canvas id="temperatures" width="300" height="100"></canvas>
+        <canvas id="moods" width="300" height="100"></canvas>
       </>
     )
   }
 }
 
-export default BarChart
+export default BarChart;
